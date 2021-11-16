ed045f113faa96dc1917059978e19144acfa065b
BB <choibobae0809@gmail.com>
2021-11-16 19:07:24 +0900

    Update /c/boj/13560 to add references

--- c/boj/13560.c
+++ c/boj/13560.c
@@ -1,7 +1,9 @@
 // 버블정렬
 // https://thrillfighter.tistory.com/209
 
+// nC2 -> n! / 2! * (n - 2)!
 // Landau's theorem : tournament graph
+// https://velog.io/@deepen/13560-%EC%B6%95%EA%B5%AC-%EA%B2%8C%EC%9E%84
 // https://travelbeeee.tistory.com/14
 // https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=jh05013&logNo=221001524338
 
