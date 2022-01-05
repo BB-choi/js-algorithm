a1c615afbe15304282927df67248289e6f5853a7
BB <choibobae0809@gmail.com>
2022-01-05 20:11:39 +0900

    Update 10757 - 큰 수 A + B

--- javascript/boj/10757.js
+++ javascript/boj/10757.js
@@ -1,7 +1,7 @@
 // 10757 - 큰 수 A+B
 
 let path = "/dev/stdin";
-// path = "input.txt";
+path = "input.txt";
 
 let [A, B] = require("fs")
     .readFileSync(path)
@@ -10,7 +10,27 @@ let [A, B] = require("fs")
     .split(" ")
     .map((el) => el.split("").reverse().map(Number));
 
-let lenA = A.length;
+let [longN, shortN] = A.length > B.length ? [A, B] : [B, A];
+
+let carry = 0;
+let result = [];
+for (let i = 0; i < longN.length; i++) {
+    let sum = longN[i] + (shortN[i] ? shortN[i] : 0) + carry;
+    carry = 0;
+
+    if (sum > 9) {
+        carry = 1;
+        sum -= 10;
+    }
+    result.push(sum);
+}
+if (carry) {
+    result.push(carry);
+}
+
+console.log(result.reverse().join(""));
+
+/* let lenA = A.length;
 let lenB = B.length;
 
 let maxLen = lenA > lenB ? lenA : lenB;
@@ -49,4 +69,4 @@ if (carry) {
     result.push(carry);
 }
 
-console.log(result.reverse().join(""));
+console.log(result.reverse().join("")); */
