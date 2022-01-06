a3f7c8c3fc9d35499d8b381e2aab989e681d1809
BB <choibobae0809@gmail.com>
2022-01-06 09:58:34 +0900

    Add Lv1. K번째 수

---
+++ javascript/programmers/level-1/3-Kth-number.js
@@ -0,0 +1,32 @@
+// K번째수
+// https://programmers.co.kr/learn/courses/30/lessons/42748
+
+function solution(array, commands) {
+    let testCnt = commands.length;
+    var answer = [];
+
+    for (let idx = 0; idx < testCnt; idx++) {
+        let [i, j, k] = commands[idx];
+        i--;
+        j--;
+        k--;
+
+        let arr = array.slice(i, j + 1);
+
+        arr.sort((a, b) => a - b);
+        answer.push(arr[k]);
+    }
+
+    return answer;
+}
+
+console.log(
+    solution(
+        [1, 5, 2, 6, 3, 7, 4],
+        [
+            [2, 5, 3],
+            [4, 4, 1],
+            [1, 7, 3],
+        ]
+    )
+);
