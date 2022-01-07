3792d9ed6bb9c4c21188ce324f6f342adc6495b3
BB <choibobae0809@gmail.com>
2022-01-07 23:13:56 +0900

    Add Lv1. 크레인 인형뽑기 게임

---
+++ javascript/programmers/level-1/7-claw-machine.js
@@ -0,0 +1,44 @@
+// 크레인 인형뽑기 게임
+// https://programmers.co.kr/learn/courses/30/lessons/64061
+
+function solution(board, moves) {
+    var answer = 0;
+    let box = [];
+    let boardN = board[0].length;
+    let cnt = 0;
+
+    for (let n of moves) {
+        let doll = -1;
+        n--;
+        for (let i = 0; i < boardN; i++) {
+            if (board[i][n]) {
+                doll = board[i][n];
+                board[i][n] = 0;
+                break;
+            } else {
+            }
+        }
+
+        if (box.length && box[box.length - 1] === doll) {
+            box.pop();
+            cnt += 2;
+        } else if (doll !== -1) {
+            box.push(doll);
+        }
+    }
+    answer = cnt;
+    return answer;
+}
+
+console.log(
+    solution(
+        [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 3],
+            [0, 2, 5, 0, 1],
+            [4, 2, 4, 4, 2],
+            [3, 5, 1, 3, 1],
+        ],
+        [1, 5, 3, 5, 1, 2, 1, 4]
+    )
+);
