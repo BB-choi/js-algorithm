97f6c4262ae02bcb9e9ee0b3048f5c03b4142bcc
BB-choi <choibobae0809@gmail.com>
2022-03-15 07:29:24 +0900

    Add 217. Contains Duplicate

---
+++ javascript/leetcode/217-Contains-Duplicate.js
@@ -0,0 +1,20 @@
+// 217. Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let map = new Map();
+
+  for (let x of nums) {
+    if (map.has(x)) return true;
+    else map.set(x, 1);
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
