c5c94714504a022919c3f09c7792f2fb2474458f
BB-choi <choibobae0809@gmail.com>
2022-05-10 09:45:49 +0900

    Lv2. 최솟값 만들기

---
+++ javascript/programmers/level-2/33-최솟값-만들기.js
@@ -0,0 +1,21 @@
+// 최솟값 만들기
+// https://programmers.co.kr/learn/courses/30/lessons/12941
+
+function solution(A, B) {
+  let answer = 0;
+
+  const lenA = A.length;
+  const lenB = B.length;
+
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  answer = A.reduce((prev, cur, idx) => {
+    return prev + cur * B[idx];
+  }, 0);
+
+  return answer;
+}
+
+console.log(solution([1, 4, 2], [5, 4, 4]));
+console.log(solution([1, 2], [3, 4]));
