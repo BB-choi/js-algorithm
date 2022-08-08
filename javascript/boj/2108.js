d16d041894285f7d0f4c4fe294b25ff3b9b9c3cf
BB-choi <choibobae0809@gmail.com>
2022-08-08 12:54:11 +0900

    Update 2108 -  통계학

--- javascript/boj/2108.js
+++ javascript/boj/2108.js
@@ -1,12 +1,11 @@
-let input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n");
+// 통계학
+// https://www.acmicpc.net/problem/2108
 
-// let input = ["5", "1", "3", "8", "-2", "2"];
-// let input = ["1", "4000"];
-// let input = ["5", "-1", "-2", "-3", "-1", "-2"];
+let path = "input.txt";
+// path = "/dev/stdin";
+
+let input = require("fs").readFileSync(path).toString().trim().split("\n");
+
 let arr = input.map(Number);
 arr.shift();
 let [avg, center, diff] = [0, 0, 0];
@@ -14,7 +13,7 @@ let len = arr.length;
 
 let sum = 0;
 for (let i = 0; i < len; i++) {
-    sum += arr[i];
+  sum += arr[i];
 }
 avg = Math.round(sum / len);
 
@@ -24,24 +23,24 @@ center = arrSort[Math.floor(len / 2)];
 
 const numMap = new Map();
 for (let n of arrSort) {
-    if (numMap.has(n)) {
-        numMap.set(n, numMap.get(n) + 1);
-    } else numMap.set(n, 1);
+  if (numMap.has(n)) {
+    numMap.set(n, numMap.get(n) + 1);
+  } else numMap.set(n, 1);
 }
 
 let getOften = [];
 let maxOftenValue = 0;
 numMap.forEach((val, key) => {
-    if (val > maxOftenValue) {
-        maxOftenValue = val;
-        // 기존 maxOftenValue보다 큰 것을 발견했으므로 배열을 비워준다
-        getOften = [];
-        getOften.push(key);
-    } else if (maxOftenValue === numMap.get(key)) {
-        getOften.push(key);
-    }
+  if (val > maxOftenValue) {
+    maxOftenValue = val;
+    // 기존 maxOftenValue보다 큰 것을 발견했으므로 배열을 비워준다
+    getOften = [];
+    getOften.push(key);
+  } else if (maxOftenValue === numMap.get(key)) {
+    getOften.push(key);
+  }
 });
 
 let often = getOften.length > 1 ? getOften[1] : getOften[0];
 
-console.log(avg, center, often, diff);
+console.log(`${avg}\n${center}\n${often}\n${diff}`);
